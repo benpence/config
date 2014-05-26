7c44dd9fe962f57b596001a745c00a97357e724e
Ben Pence <github@benpence.com>
2014-05-26 09:23:51 -0500

    Emacs: Restores evil-mode tab key to do its normal emacs function

--- home/.emacs.d/lisp/evil-mode.el
+++ home/.emacs.d/lisp/evil-mode.el
@@ -1,6 +1,14 @@
 (require 'evil)
 (evil-mode 1)
 
+(defun evil-undefine ()
+ "Leave some bindings alone.
+When used in conjunction with `define-key', it calls the original binding.
+Credit: http://dnquark.com/blog/2012/02/emacs-evil-ecumenicalism/"
+ (interactive)
+ (let (evil-mode-map-alist)
+   (call-interactively (key-binding (this-command-keys)))))
+
 (setcdr evil-insert-state-map nil) ; clears insert state bindigs so insert state 
 (define-key evil-insert-state-map  ; is basically emacs state
   (read-kbd-macro evil-toggle-key)
@@ -9,3 +17,7 @@
 (define-key evil-insert-state-map  ; set C-z to switch from insert state back to
   "\C-z"                           ; normal state
   'evil-normal-state)
+
+(define-key evil-normal-state-map  ; ignore tab in normal state
+  (kbd "TAB")
+  'evil-undefine)
