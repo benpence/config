c8ca06fac825dcd4f5f8c0c79965f13c8f83ce91
Ben Pence <github@benpence.com>
2014-08-05 10:15:22 -0400

    Emacs: Adds new agenda file

--- home/.emacs.d/lisp/org-mode.el
+++ home/.emacs.d/lisp/org-mode.el
@@ -20,6 +20,7 @@
                                   "~/Documents/me/wiki/Projects.org"
                                   "~/Documents/me/wiki/Travel.org"
                                   "~/Documents/me/wiki/Spark.org"
+                                  "~/Documents/me/wiki/Recurring.org"
                                  )
   org-agenda-format-date        "%d-%m-%Y"
   ; Agenda starts on today
