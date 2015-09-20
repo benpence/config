0a75df412a013d6f515d011823ddfebf94c328f8
Ben Pence <github@benpence.com>
2015-09-19 17:49:01 -0700

    X: Refactors Xmonad config

--- home/.xmonad/xmonad.hs
+++ home/.xmonad/xmonad.hs
@@ -1,76 +1,83 @@
 import System.IO
 
 import XMonad
-import XMonad.Util.EZConfig(additionalKeys)
-import XMonad.Util.EZConfig(removeKeys)
-import XMonad.StackSet(sink)
-
-myManageHook = composeAll
-    [ className =? "Gimp"     --> doShift "3"
-    , className =? "Inkscape" --> doShift "3" ]
-
--- Modifier is Alt
-modifier = mod4Mask
+import XMonad.Util.EZConfig (additionalKeys)
+import XMonad.Util.EZConfig (removeKeys)
+import XMonad.StackSet (sink)
 
 main = do
     -- Run WM-independent startup commands
     spawn "~/.xinitrc"
 
-    xmonad $ defaultConfig
-        -- Terminal to launch on Mod-Shift-Enter
-        { terminal           = "urxvt"
-        , workspaces         = ["1", "2", "3"]
-        , modMask            = modifier
-        
-        -- Window borders
-        , borderWidth        = 1
-        , normalBorderColor  = "#000000"
-        , focusedBorderColor = "#333333"
+    xmonad $ properties
+        `removeKeys`     removedKeys
+        `additionalKeys` addedKeys
 
-        -- Remove specific default keys
-        } `removeKeys`
-            -- This has been changed to mod-shift-p
-            [ (modifier, xK_p)
+properties = defaultConfig
+  -- Terminal to launch on Mod-Shift-Enter
+  { terminal           = "urxvt"
+  , workspaces         = ["1", "2", "3"]
+  , modMask            = modifier
 
-            -- Removed to ease emacs compatibility
-            , (modifier, xK_j)
-            , (modifier, xK_k)
+  -- Change
+  , manageHook         = composeAll
+    [ className =? "MPlayer" --> (ask >>= doF . sink)
+    ]
 
-            , (modifier, xK_Return)
+  -- Window borders
+  , borderWidth        = 1
+  , normalBorderColor  = "#000000"
+  , focusedBorderColor = "#333333"
 
-            , (modifier, xK_w)
-            , (modifier, xK_e)
-            , (modifier, xK_r)
-            , (modifier .|. shiftMask, xK_w)
-            , (modifier .|. shiftMask, xK_e)
-            , (modifier .|. shiftMask, xK_r)
+  -- Remove specific default keys
+  }
+{- HOTKEYS -}
 
-            , (modifier, xK_t)
-
-        -- Hotkeys
-        ] `additionalKeys`
-              -- Lock screensaver button
-            [ ((modifier .|. shiftMask, xK_l),  spawn "xscreensaver-command -lock")
-
-              -- Dmenu app launcher with added font
-            , ((modifier .|. shiftMask, xK_p),  spawn "dmenu_run")
-
-              -- New combination for 'unfloating' (sink) a floating window back into the layout manager
-            , ((modifier .|. shiftMask, xK_t),  withFocused $ windows . sink)
+modifier = mod4Mask
 
 
-              -- Music playback
-            , ((0, xK_F1),                      spawn "mpc prev")
-            , ((0, xK_F2),                      spawn "mpc next")
-            , ((0, xK_F3),                      spawn "mpc stop && mpc play")
-            , ((0, xK_F4),                      spawn "mpc toggle && $HOME/bin/musicPlayback")
+addedKeys =
+  -- Lock screensaver button
+  [ ((modifier .|. shiftMask, xK_l),  spawn "$HOME/bin/lock")
 
-              -- Adjust backlight
-            , ((0, xK_F6),                      spawn "xbacklight -3 -time 20")
-            , ((0, xK_F7),                      spawn "xbacklight +3 -time 20")
+  -- Dmenu app launcher with added font
+  , ((modifier .|. shiftMask, xK_p),  spawn "dmenu_run")
 
-              -- Volume mute, down, up for F8 F9 F10
-            , ((0, xK_F8),                      spawn "$HOME/bin/volume toggle")
-            , ((0, xK_F9),                      spawn "$HOME/bin/volume down")
-            , ((0, xK_F10),                     spawn "$HOME/bin/volume up")
-            ]
+  -- New combination for 'unfloating' (sink) a floating window back into the layout manager
+  , ((modifier .|. shiftMask, xK_t),  withFocused $ windows . sink)
+
+  -- Music playback
+  , ((controlMask, xK_F1),            spawn "mpc prev")
+  , ((controlMask, xK_F2),            spawn "mpc next")
+  , ((controlMask, xK_F3),            spawn "mpc stop && mpc play")
+  , ((controlMask, xK_F4),            spawn "mpc toggle && $HOME/bin/musicPlayback")
+
+  -- Adjust backlight
+  , ((0, xK_F5),                      spawn "xbacklight -3 -time 20")
+  , ((0, xK_F6),                      spawn "xbacklight +3 -time 20")
+
+  -- Volume mute, down, up for F8 F9 F10
+  , ((0, xK_F8),                      spawn "$HOME/bin/volume toggle")
+  , ((0, xK_F9),                      spawn "$HOME/bin/volume down")
+  , ((0, xK_F10),                     spawn "$HOME/bin/volume up")
+  ]
+
+removedKeys =
+  -- This has been changed to mod-shift-p
+  [ (modifier, xK_p)
+
+  -- Removed to ease emacs compatibility
+  , (modifier, xK_j)
+  , (modifier, xK_k)
+
+  , (modifier, xK_Return)
+
+  , (modifier, xK_w)
+  , (modifier, xK_e)
+  , (modifier, xK_r)
+  , (modifier .|. shiftMask, xK_w)
+  , (modifier .|. shiftMask, xK_e)
+  , (modifier .|. shiftMask, xK_r)
+
+  , (modifier, xK_t)
+  ]
